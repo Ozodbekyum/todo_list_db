1179fa9738bcef1f278b94543619ac3a6d8d98f1
Ozodbekyum <hello7ozod@gamil.com>
2023-02-03 16:42:06 +0500

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todo_list_db/screens/add_task_page.dart';
+import 'package:todo_list_db/screens/task_page.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController _controller = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Store a Data'),
-          ),
-          body: Column(
-            children: [
-              TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  hintText: 'Enter a some text',
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () async {
-                    final prefs = await SharedPreferences.getInstance();
-                    final counter = prefs.getInt('counter') ?? 0;
-
-                    print(counter.runtimeType);
-                  },
-                  child: Text('Get Data')),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () async {
-              final prefs = await SharedPreferences.getInstance();
-              await prefs.setInt('counter', int.parse(_controller.text));
-            },
-            child: const Icon(Icons.add),
-          )),
-    );
-  }
+  runApp(
+    MaterialApp(debugShowCheckedModeBanner: false,
+      initialRoute: 'open',
+      routes: {
+        'addTask': (context) => MyApp(),
+        'open':(context) => TaskPage(),
+      },
+    ),
+  );
 }
---
+++ lib/models/model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Change {
+  String title;
+  Icon icon;
+  int numbers;
+
+  Change({
+    required this.icon, 
+    required this.numbers, 
+    required this.title});
+}
