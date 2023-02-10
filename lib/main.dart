668afa38b42f2eb8d5505fe68d7171f03406db4a
Ozodbekyum <hello7ozod@gamil.com>
2023-02-10 15:33:39 +0500

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:todo_list_db/screens/home_page.dart';
 import 'package:todo_list_db/screens/login_page.dart';
+import 'package:todo_list_db/screens/signup_page.dart';
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: 'home',
       routes: {
-        
         'home': (context) => HomePage(),
-        // 'tasks': (context) => PageAll(),
-        'login':(context) => LoginPage(),
+        'signup': (context) => SignUp(),
+        'login': (context) => LoginPage(),
       },
     ),
   );
