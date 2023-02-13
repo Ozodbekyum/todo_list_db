c1b1fbed2dac9557a33bf65a53febfc73ded6055
Ozodbekyum <hello7ozod@gamil.com>
2023-02-13 16:39:53 +0500

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_db/screens/all_tasks..dart';
 import 'package:todo_list_db/screens/home_page.dart';
 import 'package:todo_list_db/screens/login_page.dart';
 import 'package:todo_list_db/screens/signup_page.dart';
@@ -12,6 +13,7 @@ void main() {
         'home': (context) => HomePage(),
         'signup': (context) => SignUp(),
         'login': (context) => LoginPage(),
+        'tasks':(context) => TaskPage(),
       },
     ),
   );
