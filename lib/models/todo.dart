5b39fd728e58c1241877e20ecbac3478a549399d
Ozodbekyum <hello7ozod@gamil.com>
2023-02-13 16:39:27 +0500

    init

---
+++ lib/models/todo.dart
@@ -0,0 +1,13 @@
+class Todo {
+  final String title;
+  final String description;
+  final bool completed;
+  final int id;
+
+  Todo({
+    required this.title,
+    required this.description,
+    required this.completed,
+    required this.id,
+  });
+}
